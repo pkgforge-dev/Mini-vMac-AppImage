d050f25b34e581ed0d66c956ea9207d50ce24dd2
Link4Electronics <link4electronics@gmail.com>
2026-01-30 23:46:57 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -26,28 +26,32 @@ REPO="https://github.com/minivmac/minivmac.git"
 GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
 
 # Determine to build nightly or stable
-if [ "${DEVEL_RELEASE-}" = 1 ]; then
-	echo "Making nightly build of Mini vMac..."
+#if [ "${DEVEL_RELEASE-}" = 1 ]; then
+#	echo "Making nightly build of Mini vMac..."
+#	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+#	git clone "$REPO" ./minivmac
+#else
+#	echo "Making stable build of Mini vMac..."
+#	wget "$GRON" -O ./gron.awk
+#	chmod +x ./gron.awk
+#	VERSION=$(wget https://api.github.com/repos/minivmac/minivmac/tags -O - | \
+#		./gron.awk | grep -v "nJoy" | awk -F'=|"' '/name/ {print $3}' | \
+#		sort -V -r | head -1)
+#	git clone --branch "$VERSION" --single-branch "$REPO" ./minivmac
+#fi
+
+echo "Making nightly build of Mini vMac..."
 	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
 	git clone "$REPO" ./minivmac
-else
-	echo "Making stable build of Mini vMac..."
-	wget "$GRON" -O ./gron.awk
-	chmod +x ./gron.awk
-	VERSION=$(wget https://api.github.com/repos/minivmac/minivmac/tags -O - | \
-		./gron.awk | grep -v "nJoy" | awk -F'=|"' '/name/ {print $3}' | \
-		sort -V -r | head -1)
-	git clone --branch "$VERSION" --single-branch "$REPO" ./minivmac
-fi
 echo "$VERSION" > ~/version
 
 # BUILD Mini vMac
 cd ./minivmac
 mkdir -p bin
 gcc setup/tool.c -o setup_t
-ls
-#source build_linux.sh
-#make -j $(nproc)
+#ls
+source build_linux.sh
+make -j $(nproc)
 
 # icon and desktop entry
 install -Dm644 .AppDir/minivmac.desktop /usr/share/applications/minivmac.desktop
